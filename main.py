759cb901f29f40e2f7280a0a6cbd41c53e6236f3
ayratzay <Ayratzay@gmail.com>
2015-05-13 15:18:50 +0300

    change items return all inventory player chooses best move based only his own utility

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ __author__ = 'AZaynutdinov'
 ############ settting game #################
 
 from random import randint, sample
+from copy import deepcopy
 
 players = 2
 items = 3
@@ -17,25 +18,16 @@ inventory = [list(args) for args in zip(*item_ownership)]
 ########### evaluating game results ###############
 
 
-final_score = [sum([m * i for m, i in zip (mt, iy)]) for mt,iy in zip(merit, inventory)]
-all_scores = sorted(list(set(final_score)), reverse = True)
-
-
-for score in all_scores:
-    for index, fs in enumerate(final_score):
-        if score == fs:
-            print (score,index)
-
-
 def change_items(A_side, B_side, A_side_item, B_side_item):
+    new_state = deepcopy(inventory) # creating new list object # deepcopy works slow
     if inventory[A_side][A_side_item]:
         if inventory[B_side][B_side_item]:
-            inventory[A_side][A_side_item] = 0
-            inventory[B_side][B_side_item] = 0
-            inventory[A_side][B_side_item] = 1
-            inventory[B_side][A_side_item] = 1
-            return inventory[A_side]
-    return False
+            new_state[A_side][A_side_item] = 0
+            new_state[B_side][B_side_item] = 0
+            new_state[A_side][B_side_item] = 1
+            new_state[B_side][A_side_item] = 1
+            return new_state
+    return new_state
 
 inventory
 change_items(0, 0 , 0, 0)
@@ -47,7 +39,7 @@ def all_moves(me, others):   # 0, [1]
     :param others: list of ints
     :return: list
     """
-    possibilities = []
+    possibilities = [[0,0,0,0, change_items]]   # does nothing
     for my_index, my_item in enumerate(inventory[me]):
         for person in others:
             for others_index, others_item in enumerate(inventory[person]):
@@ -56,30 +48,21 @@ def all_moves(me, others):   # 0, [1]
                         possibilities.append([me, person, my_index, others_index, change_items]) #at the moment only item exchange
     return possibilities
 
-all_moves(0, [1])
+new_states = [move[-1](*move[0:-1]) for move in all_moves(0, [1])]
 
-def score_inventory(person, inventory):
-    return sum([a*b for a,b in zip(merit[person], inventory)])
-
-score_inventory(0, [1,1,1])
-score_inventory(1, [1,1,1])
-
-def evaluate_move(possible_moves):
-    """
-    Gets all possible turns for player and evaluates utility(merit). Returns the turn with highest utility. If current
-    state has most utility returns True
-    :param possible_moves: list of list
-    :return: list
-    """
-    possible_moves.append([possible_moves[0][0], possible_moves[0][0], 0, 0, change_items]) # player skips turn
-    print [move[-1](*move[0:-1]) for move in possible_moves]
+first_side_decision = max([(merit[0], state[0]) for state in new_states], key = lambda tpl: sum(m*s for m,s in zip(tpl[0], tpl[1])))
 
 
-inventory
-evaluate_move(all_moves(0, [1]))
+#
+# STATE
+# PLAYER 0 checks all possible moves
+# PLAYER 0 chooses best 3 moves for him
+# PLAYER 0 suggests his best deal to conterpart
+# conterpart decides if he accetps it or not
+# if accepts
+#     change
+#     next turn
+# else:
+#     make next valuable suggestion
+#
 
-def make_turn(move):
-    move[-1](*move[0:-1])
-    return True
-
-make_turn(move)
